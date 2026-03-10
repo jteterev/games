4f259df4c03170db77b208f5beffde3b663012ea
Евгений <teterevevgeniy2002@gmail.com>
2026-03-10 11:13:52 +0700

    ColorGen

---
+++ js/ColorGenerator.js
@@ -0,0 +1,14 @@
+function startColorGenerator(buttonElement) {
+
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    const color = `#${r.toString(16).padStart(2, '0')}${g.toString(16).padStart(2, '0')}${b.toString(16).padStart(2, '0')}`;
+    
+    document.querySelector('div.page-wrapper').style.backgroundColor = color;
+    
+    alert('Цвет изменён!\n' + 
+      'Новый цвет: ' + color + '\n' + 
+      'RGB: (' + r + ', ' + g + ', ' + b + ')\n' + 
+      '— — — — — — — — —');
+};
